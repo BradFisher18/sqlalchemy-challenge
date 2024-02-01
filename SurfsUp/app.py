8bd02e73e920881e146ccb3cdda8f95f376571f2
BradFisher18 <bradfisher8@hotmail.com>
2024-02-01 11:12:32 +0800

    Empty Upload

---
+++ SurfsUp/app.py
@@ -0,0 +1,32 @@
+# Import the dependencies.
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+
+# Declare a Base using `automap_base()`
+
+# Use the Base class to reflect the database tables
+
+
+# Assign the measurement class to a variable called `Measurement` and
+# the station class to a variable called `Station`
+
+
+# Create a session
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
